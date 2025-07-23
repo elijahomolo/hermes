400af6e68399a2d0c97fc2aba78355ec00137b8c
elijahomolo <eomolo11@gmail.com>
2025-07-23 20:08:30 +0200

    users

---
+++ users/model.go
@@ -0,0 +1,14 @@
+package user 
+
+import "time" 
+
+type User struct {
+   ID string 
+   FirstName string 
+   LastName string 
+   DateOfBirth time.Time 
+   Country string 
+   Language string 
+   Email string 
+   CreatedAt time.time
+}
