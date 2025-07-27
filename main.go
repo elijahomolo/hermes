5339bd1e0cbc46c114aecb094570247d925785ea
elijahomolo <eomolo11@gmail.com>
2025-07-27 18:46:07 +0200

    db connecton

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	mysql "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-    fmt.Println("Welcome to Hermes")
+	godotenv.Load(".env")
+	cfg := mysql.Config{
+		User:                 os.Getenv("DATABASE_USER"),
+		Passwd:               os.Getenv("DATABASE_PASSWORD"),
+		Net:                  "tcp",
+		Addr:                 os.Getenv("DATABASE_ADDRESS"),
+		DBName:               os.Getenv("DATABASE_NAME"),
+		AllowNativePasswords: true,
+	}
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to MySQL!")
 }
